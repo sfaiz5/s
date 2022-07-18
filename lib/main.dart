602c11a79470d3e3245f9e3465eaaac8f59a810d
sfaiz15 <107978726+sfaiz15@users.noreply.github.com>
2022-07-18 00:17:23 -0400

    commit on forms

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:create_social/pages/authentication.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+import 'package:flutter/material.dart';
+
+import 'dart:async';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(const SocialApp());
+}
+
+class SocialApp extends StatelessWidget {
+  const SocialApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'SocialApp',
+        theme: ThemeData(
+          primarySwatch: Colors.green,
+        ),
+        home: const Authentication());
+  }
+}
---
+++ lib/pages/authentication.dart
@@ -0,0 +1,14 @@
+import 'package:create_social/forms/loginform.dart';
+import 'package:flutter/material.dart';
+
+class Authentication extends StatelessWidget {
+  const Authentication({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("Login to your account!"),
+        ),
+        body: const LoginForm());
+  }
+}
